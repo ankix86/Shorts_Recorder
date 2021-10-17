7925ee17c5e7c0ee12bfe93c318a001e42f87051
ankix86 <ankix99@gmail.com>
2021-10-17 13:35:23 +0530

    Added Comments and Organized Code

--- Recorder.py
+++ Recorder.py
@@ -8,24 +8,14 @@ from threading import Thread, Event
 import threading
 import PySimpleGUI as sg
 import time
-from PIL import Image
 import keyboard
 from mss import factory, mss
 from numba import njit, jit, cuda
 from multiprocessing import Queue, Process
-from PIL import Image, ImageTk
 from pycparser.c_ast import While
 
-#Code isn't Oraganized very well.
-#This Program build for record YT-Shorts/Reels Techy videos Without. 
-#Current Features : Record Video in [AVI] format.
-#Future Feature : Audio : Mic Support / Noise Reduction
-#               : Video : More Video Output format / Filters () / Custom Logo / Text 
-
 def stopRecording():
-    global keepRecording, btnStartRecording,btnStopRecording,frame_position,screen_size
-    pointer_in = False
-    frame_position = [0,0]
+    global keepRecording, btnStartRecording,btnStopRecording,screen_size
     keepRecording = False
     btnStartRecording["state"] = "normal"
     btnStopRecording["state"] = "disabled"
@@ -43,37 +33,23 @@ def startRecording(monitor,SCREEN_SIZE):
     FPS = float(X*5+10)
     print(FPS)
     window.iconify()
-    #_recordScreen(FPS,monitor,keepRecording,SCREEN_SIZE)
     threading.Thread(target=recordScreen,args=[FPS,monitor,SCREEN_SIZE]).start()
-
-def runTimeWindowRenderer(monitor):
-    global frame_position
-    SCREEN_SIZE = screen_size
-    top_padding = frame_position[0]
-    left_padding =frame_position[1]
-    monitor = {"top":int(top_padding) + 30,"left":int(left_padding) - 30,"width":SCREEN_SIZE[0],"height":SCREEN_SIZE[1]}
-    return monitor
 
 def recordScreen(FPS,monitor,SCREEN_SIZE,):
     global keepRecording
-    #timeForFrame = 1/FPS
-    #img = np.zeros(SCREEN_SIZE)
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     output = cv2.VideoWriter("output.avi",fourcc,FPS,(SCREEN_SIZE))
     sct = mss()
     queue = Queue()
     threading.Thread(target=_recordScreen,args=[monitor,sct,queue]).start()
-    #Having more than one thread doesn't improve performance
-    #threading.Thread(target=_recordScreen,args=[monitor,sct,1,i,queue]).start()
     threading.Thread(target=_writeScreen,args=[output,queue]).start()
-    #output.release()
-
 
 def _recordScreen(monitor,sct,queue):
     i = 0
     start = current = time.time()
     while keepRecording == True:
-        monitor = update_monitor()[0]
+        #updating monitor 
+        monitor = update_monitor()
         queue.put(np.array(sct.grab(monitor)))
         i+=1
         if(current-start>1):
@@ -94,14 +70,18 @@ def _writeScreen(output, queue):
     output.release()
 
 def update_monitor():
-    global floater
+    #Calculating Offest X and Y for Make Screen On Center. 
     _offsetx = int(screen_size[1]/2)
     _offsety = int(screen_size[0]/2)
-    SCREEN_SIZE = screen_size
+
     top_padding = floater.frame_position[1]
     left_padding =floater.frame_position[0]
-    monitor = {"top":top_padding-_offsetx,"left":left_padding-_offsety,"width":SCREEN_SIZE[0],"height":SCREEN_SIZE[1]}
-    return monitor,SCREEN_SIZE
+
+    #top => offset_x
+    #left => offset_y
+    
+    monitor = {"top":top_padding-_offsetx,"left":left_padding-_offsety,"width":screen_size[0],"height":screen_size[1]}
+    return monitor
 
 class FloatingWindow(tk.Toplevel):
     def __init__(self, *args, **kwargs):
@@ -112,37 +92,49 @@ class FloatingWindow(tk.Toplevel):
         self.overrideredirect(True)
         self.wm_attributes("-transparentcolor", "blue")
         self.c = tk.Canvas(self,height=screen_size[0], width=screen_size[1], bg="blue")
-        self.c.pack()
+        self.c.pack(side="left", fill="y")  
+
+        #Accessing PNG file from Current Directory (You can import direct PNG file path and replace it.)
         self.filex = PhotoImage(file='s_logo.png')
-        point = self.c.create_image(0,0, image=self.filex, anchor=NW)
+        self.c.create_image(0,0, image=self.filex, anchor=NW)
+
+        #Initializing frame_position with current location of floating window or self.
         self.frame_position = self.winfo_pointerxy()    
+
+        #EVENT
         self.c.bind("<B1-Motion>", self.dragwinx)
-        self.c.pack(side="left", fill="y")   
         self.c.bind("<Button-1>", self.clickwin)
- 
-                    
+      
     def dragwinx(self, event):
+        #Call when B1-Motion or Drag event on canvas window.
         x = self.winfo_pointerx() - self._offsetx
         y = self.winfo_pointery() - self._offsety
+
+        #Calulating the canvas screen_size with in x & y offsets.
         y_p = int(self.winfo_rooty()+screen_size[0]/2)
         x_p = int(self.winfo_rootx()+screen_size[0]/2)
         self.frame_position = [x_p,y_p] 
+
+        #changeing canvas position
         self.geometry("+%d+%d" % (x, y))
 
     def clickwin(self, event):
+        #Call when Button-1 or Click event on canvas window.
+        #Sotring x & y offset values.
         self._offsetx = event.widget.winfo_rootx() - self.winfo_rootx() + event.x
         self._offsety = event.widget.winfo_rooty() - self.winfo_rooty() + event.y
 
 if __name__ == "__main__":
-    global screen_size
-    global floater
+    #Disabling or Setting False Intial Booleans 
     keepRecording = False
+
+    #Static Screen Size (500,500)
+    #screen_size Changes [s_logo.png => is 500*500px image with transparent background] 
+    #eg. screen_size=[600,600] *requires 600*600 s_logo.png* file inorder to fit with in screen recoder.
     screen_size=[500,500]
-    #outputVideo(output,converted)
+
     cv2.destroyAllWindows()
-    #output.release()
     window = tk.Tk()
-    floater = FloatingWindow(window)
     window.geometry('300x300')
     window.title("Window")
     labelFPSSetting = tk.Label(text = "FPS")
@@ -151,10 +143,13 @@ if __name__ == "__main__":
     fpsSettings['values'] = (10, 15, 20, 25, 30,35,40, 45,50,55, 60)
     fpsSettings.current(4)
     fpsSettings.place(x=90, y=80,width = 115)
-    btnStartRecording = tk.Button(window, text = "Start Recording",command = lambda : startRecording(update_monitor()[0],update_monitor()[1]) )
+    btnStartRecording = tk.Button(window, text = "Start Recording",command = lambda : startRecording(screen_size[0],screen_size[1]) )
     btnStartRecording.place(x=90, y=120)
     btnStopRecording = tk.Button(window, text = "Stop Recording", command = stopRecording)
     btnStopRecording.place(x=90,y=160)
     btnStopRecording["state"] = "disabled"
+    
+    #Creating FloatingWindow With Canvas 
+    floater = FloatingWindow(window)
     window.mainloop()
     
